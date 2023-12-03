6217ad250cc6ef0a8bef799bf5a03e4f961118c2
Andrey Anofrikov <ano-andrej@yandex.ru>
2023-12-03 14:34:40 +0300

    Lesson 6

---
+++ Lesson 6/Lesson 6 Task 2.cpp
@@ -0,0 +1,25 @@
+﻿// Lesson 6 Task 2.cpp 
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	setlocale(LC_ALL, "rus");
+	int massif[10];
+	int num, key = 0;
+	cout << "Введите 10 элементов массива:\n";
+	for (int i = 0; i < 10; i++) {
+		cin >> massif[i];
+	}
+	cout << "Введите искомое число из массива:\n";
+	cin >> num;
+	for (int i = 0; i < 10; i++) {
+		if (massif[i] == num) {
+			key = 1;
+			cout << "Индекс искомого числа: " << i << endl << "Количество повторений: " << i + 1<<endl;
+		}
+	}
+	if (key == 0) {
+		cout << "-1\n";
+	}
+}
