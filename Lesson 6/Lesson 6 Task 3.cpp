ce30ee5c97f1b9a9734278ffad7b031fed16030c
Andrey Anofrikov <ano-andrej@yandex.ru>
2023-12-04 19:47:44 +0300

    Fixed task 3 lesson 6

--- Lesson 6/Lesson 6 Task 3.cpp
+++ Lesson 6/Lesson 6 Task 3.cpp
@@ -7,7 +7,7 @@ int main()
 {
 	setlocale(LC_ALL, "rus");
 	int massif[10];
-	int key, index;
+	int key, index, iteration = 0;
 	cout << "Введите 10 последовательных элементов массива:\n";
 	for (int i = 0; i < 10; i++) {
 		cin >> massif[i];
@@ -16,6 +16,7 @@ int main()
 	cin >> key;
 	int left = 0, right = 10, search = -1;
 	while (left <= right) {
+		iteration++;
 		int mid = (left + right) / 2;
 		if (key == massif[mid]) {
 			search = massif[mid];
@@ -27,5 +28,5 @@ int main()
 		else left = mid + 1;
 	}
 	if (search == -1) cout << "-1\n" << endl;
-	else cout << "Индекс искомого элемента: " << index<<endl<<"Количество повторений "<<index+1<<endl;
+	else cout << "Индекс искомого элемента: " << index << endl << "Количество повторений " << iteration << endl;
 	}
