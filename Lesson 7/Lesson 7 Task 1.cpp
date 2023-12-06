f8ea20b2c21a55541582ef0dc718c1f54a9d9e5e
Andrey Anofrikov <ano-andrej@yandex.ru>
2023-12-06 19:35:38 +0300

    Lesson 7

---
+++ Lesson 7/Lesson 7 Task 1.cpp
@@ -0,0 +1,27 @@
+﻿// Lesson 7 Task 1.
+
+#include <iostream>
+using namespace std;
+
+int Arrey(int arr[], int size)
+{
+	int max = arr[0];
+	for (int i = 0; i < size; i++) {
+		if (arr[i] > max) {
+			max = arr[i];
+		}
+	}
+	return max;
+}
+
+int main()
+{
+	setlocale(LC_ALL, "rus");
+	int arr[10];
+	cout << "Введите 10 элементов массива:\n";
+	for (int i = 0; i < 10; i++) {
+		cin >> arr[i];
+	}
+	int a=Arrey(arr,10);
+	cout << "Максимальное число массива: " << a;
+}
---
+++ Lesson 7/Lesson 7 Task 2.cpp
@@ -0,0 +1,24 @@
+﻿// Lesson 7 Task 2.
+
+#include <iostream>
+using namespace std;
+
+double Arrey(int arr[], int size)
+{
+	double sum=0;
+	for (int i = 0; i < size; i++) {
+			sum += arr[i];
+		}
+	return sum/size;
+}
+
+int main()
+{
+	setlocale(LC_ALL, "rus");
+	int arr[10];
+	cout << "Введите 10 элементов массива:\n";
+	for (int i = 0; i < 10; i++) {
+		cin >> arr[i];
+	}
+	cout << "Среднее арифметическое элиментов массива: " << Arrey(arr, 10);
+}
