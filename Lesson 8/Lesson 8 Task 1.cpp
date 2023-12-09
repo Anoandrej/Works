44bd17c07d168ac25d417070c81732d518bfe925
Andrey Anofrikov <ano-andrej@yandex.ru>
2023-12-09 20:40:17 +0300

    Lesson 8

---
+++ Lesson 8/Lesson 8 Task 1.cpp
@@ -0,0 +1,32 @@
+﻿// Lesson 8 Task 1
+
+#include <iostream>
+using namespace std;
+
+int Ffunction (int n);
+int Gfunction (int n);
+
+int main()
+{
+    setlocale(LC_ALL, "Russian");
+
+    int n, m;
+    cout << "Введите первое число: \n";
+    cin >> n;
+    cout << "Введите второе число: \n";
+    cin >> m;
+
+    cout << "Результат выполнения функции: " << Ffunction(n) + Gfunction(m)<<endl;
+    return 0;
+}
+
+int Ffunction (int n)
+{
+    if (n == 1) return 1;
+    if (n>1) return 2 * Gfunction(n - 1) + 5 * n;
+}
+int Gfunction (int n)
+{
+    if (n == 1) return 1;
+    if (n > 1) return Ffunction(n - 1) + 2 * n;
+}
---
+++ Lesson 8/Lesson 8 Task 3.cpp
@@ -0,0 +1,25 @@
+﻿// Lesson 8 Task 3.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
+
+
+#include <iostream>
+using namespace std;
+
+int Ffunction(int n);
+
+int main()
+{
+    setlocale(LC_ALL, "Russian");
+
+    int n, m;
+    cout << "Введите число: \n";
+    cin >> n;
+
+    cout << "Результат выполнения функции: " << Ffunction(n) << endl;
+    return 0;
+}
+
+int Ffunction(int n)
+{
+    if (n == 1||n==2) return 1;
+    if (n > 2) return Ffunction(n - 2)*n;
+}
