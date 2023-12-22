b58297d915b005958ad1b2586f8c1a313b64b1c0
Andrey Anofrikov <ano-andrej@yandex.ru>
2023-12-22 18:34:45 +0300

    Lesson 10

---
+++ Lesson 10/Lesson 9 Task 6.cpp
@@ -0,0 +1,40 @@
+﻿// Lesson 9 Task 6
+
+#define _CRT_SECURE_NO_WARNINGS
+#include <iostream>
+#include <stdio.h>
+#include <stdlib.h>
+using namespace std;
+
+int main()
+{
+	FILE* input = NULL;
+	FILE* output = NULL;
+	int c;
+
+	input = fopen("f1.txt", "r");
+	output = fopen("f2.txt", "w");
+
+	if (input == NULL)
+	{
+		cout << "Error opening the file f1.txt.\n";
+		exit(0);
+	}
+
+	if (output == NULL)
+	{
+		cout << "Error opening the file f2.txt.\n";
+		exit(0);
+	}
+
+	do
+	{
+		c = fgetc(input);
+		fprintf(output, "%c", c);
+		if (c == 's') fprintf(output, "\n");
+	} while (c != EOF);
+
+	fclose(input);
+	fclose(output);
+	return 0;
+}
