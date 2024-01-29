d653f66471055e25bbd5d1944a85a7c23be9f93a
Andrey Anofrikov <ano-andrej@yandex.ru>
2024-01-29 18:18:00 +0300

    Lesson 15

---
+++ Lesson 15/Lesson 15 Task 1.cpp
@@ -0,0 +1,34 @@
+﻿// Lesson 15 Task 1
+
+#include <iostream>
+using namespace std;
+
+struct Adder {
+public:
+	void price(double b) {
+		sum += b;
+	};
+	double totalPrice() {
+		return sum;
+	}
+private:
+	double sum;
+};
+
+
+int main()
+{
+	system("chcp 1251>nul");
+
+	Adder c;
+	int a;
+	double b;
+	cout << "Введите количество товаров: \n";
+	cin >> a;
+	for (int i = 0; i < a; i++) {
+		cout << "Введите цену " << i + 1 << " товара:\n";
+		cin >> b;
+		c.price(b);
+	}
+	cout<<"Total price: "<<c.totalPrice();
+}
