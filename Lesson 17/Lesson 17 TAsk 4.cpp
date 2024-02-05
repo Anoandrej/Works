c1d39a5e1a87bea946c2c34c4af00b1d78f98ea3
Andrey Anofrikov <ano-andrej@yandex.ru>
2024-02-05 15:54:02 +0300

    Lesson 17

---
+++ Lesson 17/Lesson 17 TAsk 4.cpp
@@ -0,0 +1,20 @@
+﻿// Lesson 17 TAsk 4
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    system("chcp 1251>nul");
+
+    string str1, str2, result;
+    cout << "Первая строка:\n";
+    cin >> str1;
+    cout << "Вторая строка:\n";
+    cin >> str2;
+    result = str1 + " " + str2;
+    cout << result;
+}
+
+//Напишите программу, которая запрашивает ввод с клавиатуры двух строковых значений(в новых строках).
+//Программа должна склеить введенные строки, вставив между ними пробел и вывести на экран.
---
+++ Lesson 17/Lesson 17 Task 2.cpp
@@ -0,0 +1,63 @@
+﻿// Lesson 17 Task 2.
+
+#include <iostream>
+//#include<string>
+//#include<cstring>
+
+using namespace std;
+
+class Person
+{
+    int age;
+protected:
+	 string name;
+
+    /*Person(string name,int age)
+    {
+        this->name = name;
+        this->age = age;
+    }*/
+	void display() {
+        cout << "Имя: " << name << " Возраст: " << age << endl;
+    }
+};
+
+class Employee :protected Person
+{
+    string company;
+
+    Employee(string company)
+    {
+        this->company = company;
+    }
+    void showEmployeeName() {
+        cout << "Имя: " << name << " Компания: " << company << endl;
+    }
+};
+
+
+int main()
+{
+	system("chcp 1251>nul");
+
+    string name, company;
+    int age;
+
+    cout << "Имя сотрудника: \n";
+    cin >> name;
+    cout<<"Возраст сотрудника: \n";
+    cin >> age;
+
+    Person firstPerson(name, age);
+    firstPerson.display();
+
+    cout << "Имя сотрудника: \n";
+    cin >> name;
+    cout << "Возраст сотрудника: \n";
+    cin >> age;
+    cout << "Название компании: \n";
+    cin >> company;
+
+    Employee firstEmploy(name, age, company);
+    firstEmploy.showEmployeeName();
+}
