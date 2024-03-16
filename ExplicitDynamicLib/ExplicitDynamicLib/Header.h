b4c8830cfbb70ef6bef316507331e0ef5b6d8d30
Andrey Anofrikov <ano-andrej@yandex.ru>
2024-03-16 17:16:25 +0300

    ExplicitDynamicLib

---
+++ ExplicitDynamicLib/ExplicitDynamicLib/Header.h
@@ -0,0 +1,3 @@
+#pragma once
+
+extern "C" __declspec(dllexport) int Sort(int a, int* arr);
---
+++ ExplicitDynamicLib/ExplicitDynamicLib/Sort.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+
+extern "C" __declspec(dllexport) int Sort(int a, int* arr) {
+
+	for (int i = 0; i < a; i++) {
+		for (int j = 0; j < a; j++) {
+			if (arr[j] < arr[j + 1]) {
+				int temp = arr[j];
+				arr[j] = arr[j + 1];
+				arr[j + 1] = temp;
+			}
+		}
+	}
+	for (int i = 0; i < a; i++) {
+		std::cout << arr[i] << " ";
+	}
+	free(arr);
+
+	return 0;
+}
