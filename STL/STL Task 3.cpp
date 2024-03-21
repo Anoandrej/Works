cbc93e89ff418314eae88b0aa51959e8334fc548
Andrey Anofrikov <ano-andrej@yandex.ru>
2024-03-21 15:17:19 +0300

    STL

---
+++ STL/STL Task 3.cpp
@@ -0,0 +1,27 @@
+﻿// STK Task 3
+#include <iostream>
+#include <list>
+#include <string>
+
+int main() {
+    std::list<std::string> browserHistory;
+    std::string input;
+
+    while (true) {
+        std::cout << "Enter the URL you visited (or 'exit' to quit): ";
+        std::cin >> input;
+
+        if (input == "exit") {
+            break;
+        }
+
+        browserHistory.push_back(input);
+    }
+
+    std::cout << "\nBrowser History:\n";
+    for (const auto& url : browserHistory) {
+        std::cout << url << "\n";
+    }
+
+    return 0;
+}
---
+++ STL/STL Task 4.cpp
@@ -0,0 +1,24 @@
+﻿// STL Task 4
+
+#include <set>
+#include <iostream>
+
+
+int main()
+{
+    std::set<int>Sort{ 1,2,3,2,4,1,5 };
+
+    for (auto& item : Sort) {
+        std::cout << item <<" ";
+    }
+    return 0;
+}
+
+// Используя контейнер set, реализуйте функцию для удаления дубликатов из списка чисел.
+//
+// Пример работы программы :
+// Входные данные :
+// {1, 2, 3, 2, 4, 1, 5}
+//
+// Выходные данные :
+// List without duplicates : 1 2 3 4 5
