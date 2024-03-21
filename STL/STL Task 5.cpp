4a3c7c162c5b62c3cb9539375c930c5bdd88990c
Andrey Anofrikov <ano-andrej@yandex.ru>
2024-03-21 21:38:16 +0300

    STL

---
+++ STL/STL Task 5.cpp
@@ -0,0 +1,35 @@
+﻿// STL Task 5
+
+#include <iostream>
+#include <set>
+using namespace std;
+
+int main()
+{
+   set < string > uniqueNames{"Alice","Bob","Charlie"};
+
+    string nameToCheck;
+    cout << "Enter name: \n";
+    cin >> nameToCheck;
+
+    if (uniqueNames.find(nameToCheck) != uniqueNames.end()) {
+        cout << nameToCheck << " is in the set.\n";
+    }
+    else {
+        cout << nameToCheck << " is not on set.\n";
+    }
+    return 0;
+}
+
+// Создайте множество, чтобы хранить уникальные имена пользователей. Проверьте, содержит ли множество конкретное имя.
+//
+// Пример работы программы :
+// Входные данные :
+// uniqueNames.insert("Alice");
+// uniqueNames.insert("Bob");
+// uniqueNames.insert("Charlie");
+//
+// std::string nameToCheck = "Alice";
+//
+// Выходные данные :
+// Alice is in the set.
---
+++ STL/STL Task 6.cpp
@@ -0,0 +1,35 @@
+﻿// STL Task 6
+
+#include <iostream>
+#include <queue>
+
+using namespace std;
+
+int main()
+{
+    std::queue<int>orderQueue;
+    orderQueue.push(101);
+    orderQueue.push(102);
+    orderQueue.push(103);
+
+    while (!orderQueue.empty())
+    {
+        int val = orderQueue.front();
+        orderQueue.pop();
+        cout << "Processing order #" << val << endl;
+    }
+    return 0;
+}
+
+// Создайте очередь для хранения номеров заказов.Реализуйте обработку заказов : извлечение заказа из очереди и печать его номера.
+//
+// Пример работы программы :
+// Входные данные :
+// orderQueue.push(101);
+// orderQueue.push(102);
+// orderQueue.push(103);
+//
+// Выходные данные :
+// Processing order #101
+// Processing order #102
+// Processing order #103
